6836b57e08d9b68c36fe4d550cd8252b942e195b
David Raj Kanthi <kanthidavid@gmail.com>
2024-03-06 11:10:28 +0530

    lombok changes made for bodel classes

---
+++ src/test/java/com/dxc/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
